9f74bd04c3ac61c992b22be108657ab2c4137812
Sebastian Rast <sebastian.rast@outlook.com>
2021-12-09 14:26:24 +0100

    the tank can drive now xD

--- Code/Tank1.gd
+++ Code/Tank1.gd
@@ -1,15 +1,30 @@
-class_name tank
-extends KinematicBody2D
+class_name Tank
+extends RigidBody2D
 
-var velocity = Vector2(0, 0)
+const WALK_ACCEL = 500.0
+const WALK_MAX_VELOCITY = 140.0
 
-func _physics_process(delta):
-	if(Input.get_action_strength("ui_right")):
-		velocity.x = 1 *  400
-	elif(Input.get_action_strength("ui_left")):
-		velocity.x = -1 * 400
-	else:
-		return
+func _integrate_forces(state):
+	var velocity = state.get_linear_velocity()
+	var step = state.get_step()
+	var on_floor = false
 	
-	velocity = move_and_slide(velocity)
-		
+	for i in range(state.get_contact_count()):
+		var contact_normal = state.get_contact_local_normal(i)
+		if contact_normal.dot(Vector2(0, -1)) > 0.5:
+			on_floor = true
+			print(on_floor)
+	
+	if on_floor:
+		if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+			velocity.x = min( velocity.x + WALK_ACCEL * step, WALK_MAX_VELOCITY )
+		elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+			velocity.x = max( velocity.x - WALK_ACCEL * step, -WALK_MAX_VELOCITY )
+		else:
+			var xspeed = max( abs(velocity.x) - WALK_ACCEL * step, 0)
+			velocity.x = xspeed * sign(velocity.x)
+
+			
+	
+	velocity += state.get_total_gravity() * step
+	state.set_linear_velocity(velocity)
