971a9e242c5baeefb595f8ee29e435accc6cd733
Sebastian Rast <sebastian.rast@outlook.com>
2021-12-20 15:06:27 +0100

    implemented conduit to right tank
    all working

---
+++ Code/Tank2.gd
@@ -0,0 +1,32 @@
+extends RigidBody2D
+
+const WALK_ACCEL = 500.0
+const WALK_MAX_VELOCITY = 140.0
+
+var collisionShapes = [$CollisionPolygon2D, $CollisionShape2D, $CollisionShape2D2, $CollisionShape2D3, $CollisionShape2D4]
+	
+		
+func _integrate_forces(state):
+	var velocity = state.get_linear_velocity()
+	var step = state.get_step()
+	var on_floor = false
+	
+	for i in range(state.get_contact_count()):
+		var contact_normal = state.get_contact_local_normal(i)
+		if contact_normal.dot(Vector2(0, -1)) > 0.5:
+			on_floor = true
+
+	if on_floor:
+		if  Input.is_action_pressed("tank2forward") and not Input.is_action_pressed("tank2backward"):
+			velocity.x = max( velocity.x - WALK_ACCEL * step, -WALK_MAX_VELOCITY )
+			print("tank 2")
+		elif Input.is_action_pressed("tank2backward") and not Input.is_action_pressed("tank2forward"):
+			velocity.x = min( velocity.x + WALK_ACCEL * step, WALK_MAX_VELOCITY )
+		else:
+			var xspeed = max( abs(velocity.x) - WALK_ACCEL * step, 0)
+			velocity.x = xspeed * sign(velocity.x)
+
+	
+
+	velocity += state.get_total_gravity() * step
+	state.set_linear_velocity(velocity)
