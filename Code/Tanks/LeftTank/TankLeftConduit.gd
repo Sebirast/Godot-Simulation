dae7d8e67c8d4d64c0ef6af214afb9593db40f14
Sebastian Rast <sebastian.rast@outlook.com>
2022-01-04 11:36:36 +0100

    deleted left conduit

--- Code/Tanks/LeftTank/TankLeftConduit.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _physics_process(delta):
-	print($conduit.get_rotation_degrees())
-	var currentAngle = $conduit.get_rotation_degrees()
-	if Input.is_action_pressed("LeftConduitUp") and not Input.is_action_pressed("LeftConduitDown"):
-		$conduit.set_rotation_degrees(currentAngle + 1)
-	if Input.is_action_pressed("LeftConduitDown") and not Input.is_action_pressed("LeftConduitUp"):
-		$conduit.set_rotation_degrees(currentAngle - 1)
